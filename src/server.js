44b539fb08d85a586ce240e3bb1734a138e6e60f
hugo <hugovarellaa@gmail.com>
2021-01-23 15:14:23 -0300

    refactoring all to use server nodejs

--- src/server.js
+++ src/server.js
@@ -2,4 +2,18 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  return res.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/study", (req, res) => {
+  return res.sendFile(__dirname + "/views/study.html");
+});
+
+app.get("/give-classes", (req, res) => {
+  return res.sendFile(__dirname + "/views/give-classes.html");
+});
+
 app.listen(3333, () => console.log("🚀🚀 Server running... 🚀🚀"));
