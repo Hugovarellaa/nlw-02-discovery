58ee1bc6a4f3c11a0293e4ef5aef2b474cc64a32
hugo <hugovarellaa@gmail.com>
2021-02-27 14:53:46 -0300

    chore: create proffys

--- src/database/db.js
+++ src/database/db.js
@@ -1,8 +1,10 @@
 const sqlite3 = require("sqlite3").verbose();
 
+// criar o objeto que irá fazer operações no banco de dados
 const db = new sqlite3.Database("./src/database/database.db");
 
-db.serialize(() => {
+// utilizar o objeto de banco de dados, para nossas operações
+db.serialize(async () => {
   //  ------------- CRIAR O BANCO DE DADOS --------------
   db.run(`
     CREATE TABLE IF NOT EXISTS proffys (
@@ -32,6 +34,46 @@ db.serialize(() => {
       time_to INTEGER NOT NULL
     );
   `);
+
+  //  ------------- INSERIR NO BANCO DE DADOS --------------
+
+  const proffy = {
+    name: "Hugo Alves Varella",
+    avatar: "http://github.com/Hugovarellaa.png",
+    whatsapp: 61995995970,
+    bio: "Alguma coisa",
+  };
+
+  const classValue = {
+    // id: Virar do pelo banco de dados ao criar os proffys
+    subject: "Desenvolvimento Web",
+    cost: "200",
+  };
+
+  const classSchedule = [
+    // class_id: virar do pelo banco de dados ao criar as classes
+    {
+      weekday: 1,
+      time_from: 720,
+      time_to: 1500,
+    },
+    {
+      weekday: 0,
+      time_from: 655,
+      time_to: 1320,
+    },
+  ];
+
+  function callback(err) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("Cadastrado com sucesso");
+  }
+
+  const query = {};
+
+  db.run(query, proffy, callback);
 });
 
 module.exports = db;
