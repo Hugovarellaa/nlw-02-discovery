a417c5379ecc7410c777a7ccc5f541dee42f25f4
hugo <hugovarellaa@gmail.com>
2021-02-28 14:54:13 -0300

    chore: config db

--- src/database/db.js
+++ src/database/db.js
@@ -1,79 +1,31 @@
-const sqlite3 = require("sqlite3").verbose();
+const Database = require("sqlite-async");
 
-// criar o objeto que irá fazer operações no banco de dados
-const db = new sqlite3.Database("./src/database/database.db");
+function execute(db) {
+  // criar as tabelas do banco de dados.
+  return db.exec(`
+        CREATE TABLE IF NOT EXISTS proffys (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            avatar TEXT,
+            whatsapp TEXT,
+            bio TEXT
+        );
 
-// utilizar o objeto de banco de dados, para nossas operações
-db.serialize(async () => {
-  //  ------------- CRIAR O BANCO DE DADOS --------------
-  db.run(`
-    CREATE TABLE IF NOT EXISTS proffys (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      name TEXT NOT NULL,
-      avatar TEXT NOT NULL,
-      whatsapp TEXT NOT NULL,
-      bio TEXT NOT NULL
-    );
-  `);
+        CREATE TABLE IF NOT EXISTS classes (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            subject INTEGER,
+            cost TEXT,
+            proffy_id INTEGER
+        );
 
-  db.run(`
-    CREATE TABLE IF NOT EXISTS classes (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      subject TEXT NOT NULL,
-      cost TEXT NOT NULL,
-      proffy_id INTEGER NOT NULL
-    );
-  `);
+        CREATE TABLE IF NOT EXISTS class_schedule (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            class_id INTEGER,
+            weekday INTEGER,
+            time_from INTEGER,
+            time_to INTEGER
+        );
+    `);
+}
 
-  db.run(`
-    CREATE TABLE IF NOT EXISTS class_schedule (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      class_id INTEGER NOT NULL,
-      weekday INTEGER NOT NULL,
-      time_from INTEGER NOT NULL,
-      time_to INTEGER NOT NULL
-    );
-  `);
-
-  //  ------------- INSERIR NO BANCO DE DADOS --------------
-
-  const proffy = {
-    name: "Hugo Alves Varella",
-    avatar: "http://github.com/Hugovarellaa.png",
-    whatsapp: 61995995970,
-    bio: "Alguma coisa",
-  };
-
-  const classValue = {
-    // id: Virar do pelo banco de dados ao criar os proffys
-    subject: "Desenvolvimento Web",
-    cost: "200",
-  };
-
-  const classSchedule = [
-    // class_id: virar do pelo banco de dados ao criar as classes
-    {
-      weekday: 1,
-      time_from: 720,
-      time_to: 1500,
-    },
-    {
-      weekday: 0,
-      time_from: 655,
-      time_to: 1320,
-    },
-  ];
-
-  function callback(err) {
-    if (err) {
-      return console.log(err);
-    }
-    console.log("Cadastrado com sucesso");
-  }
-
-  const query = {};
-
-  db.run(query, proffy, callback);
-});
-
-module.exports = db;
+module.exports = Database.open(__dirname + "/database.sqlite").then(execute);
