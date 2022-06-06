38176691bca7fa428ce10dba9308312be0e70236
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 <hugovarellaa@gmail.com>
2022-06-06 12:37:06 -0300

    Configurando Debug

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,15 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (request, response) => {
   return response.json({ message: "Ola mundo!" });
+});
+
+app.post("/courses", (request, response) => {
+  const { name } = request.body;
+  return response.json({ name });
 });
 
 app.listen(3333, () => console.log("Server started on port 3333"));
