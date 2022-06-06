988a60e8e7f1896d776b202501b024116dfacecd
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 <hugovarellaa@gmail.com>
2022-06-06 12:21:53 -0300

    Testando chamadas na aplicação

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,8 @@ import express from "express";
 
 const app = express();
 
+app.get("/", (request, response) => {
+  return response.json({ message: "Ola mundo!" });
+});
+
 app.listen(3333, () => console.log("Server started on port 3333"));
