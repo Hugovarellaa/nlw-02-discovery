dbe1dd133259ef021f4585c794a4307e291642cf
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 <hugovarellaa@gmail.com>
2022-06-07 13:15:14 -0300

    Refatorando usando Singleton pattern

---
+++ src/modules/cars/useCases/ListCategory/index.ts
@@ -0,0 +1,12 @@
+import { CategoriesRepository } from "../../repositories/CategoriesRepository";
+import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
+import { ListCategoryController } from "./ListCategoryController";
+
+const categoriesRepository = CategoriesRepository.getInstance();
+
+const listCategoriesUseCase = new ListCategoriesUseCase(categoriesRepository);
+const listCategoriesController = new ListCategoryController(
+  listCategoriesUseCase
+);
+
+export { listCategoriesController };
