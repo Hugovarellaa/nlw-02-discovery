c3a363b2a0bd3f1036e7703568ce245222f8145f
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 <hugovarellaa@gmail.com>
2022-06-07 11:16:36 -0300

    Refatorando Categories

--- src/modules/cars/useCases/CreateCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/CreateCategory/CreateCategoryUseCase.ts
@@ -1,11 +1,11 @@
-import { ICategoriesRepository } from "../repositories/ICategoriesRepository";
+import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
 
 interface IRequest {
   name: string;
   description: string;
 }
 
-class CreateCategoryServices {
+class CreateCategoryUseCase {
   constructor(private categoriesRepository: ICategoriesRepository) {}
   execute({ name, description }: IRequest): void {
     const categoryAlreadyExists = this.categoriesRepository.findByName(name);
@@ -18,4 +18,4 @@ class CreateCategoryServices {
   }
 }
 
-export { CreateCategoryServices };
+export { CreateCategoryUseCase };
