c8f7a6d1a14e97a96b88b00c0314fc0fe9afc774
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 <hugovarellaa@gmail.com>
2022-08-04 10:23:01 -0300

    Cadastro de categoria

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 import express from "express";
 
-const app = express();
+import { categoriesRouter } from "./routes/Categories.routes";
 
-app.get("/", (req, res) => {
-  return res.json({ message: "World" });
-});
+const app = express();
+app.use(express.json());
+app.use(categoriesRouter);
 
 app.listen(3333, () => console.log("Server started on port 3333"));
