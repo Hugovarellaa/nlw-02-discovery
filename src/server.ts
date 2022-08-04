712eb77fcf83ebb35cabc8e2523acafc346ab748
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-08-04 16:06:47 -0300

    Delete src directory

--- src/server.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-import { categoriesRouter } from "./routes/Categories.routes";
-
-const app = express();
-app.use(express.json());
-app.use("/categories", categoriesRouter);
-
-app.listen(3333, () => console.log("Server started on port 3333"));
