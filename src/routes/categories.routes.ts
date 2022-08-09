a08e65210377b84298ce0d93ab6d9ee50bc66911
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 <hugovarellaa@gmail.com>
2022-08-09 16:46:13 -0300

    Validando categories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -7,17 +7,20 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  // const categoriesAlreadyExists = categories.find(
-  //   (category) => category.name === name
-  // );
-
-  // if (categoriesAlreadyExists) {
-  //   return response.status(400).json({ error: "Category already exists!" });
-  // }
-
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
+  if (categoryAlreadyExists) {
+    return response.status(400).json({ error: "Category already exists!" });
+  }
+  
   categoriesRepository.create({ name, description });
 
   return response.status(201).send();
+});
+
+categoriesRoutes.get("/", (request, response) => {
+  const all = categoriesRepository.list();
+
+  return response.json(all);
 });
 
 export { categoriesRoutes };
