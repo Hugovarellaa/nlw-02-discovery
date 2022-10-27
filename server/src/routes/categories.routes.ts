1f661c5081e452819691b478b2867b4dbbcb291f
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-10-27 18:01:45 -0300

    Refatorando specification

--- server/src/routes/categories.routes.ts
+++ server/src/routes/categories.routes.ts
@@ -1,10 +1,8 @@
 import { Router } from 'express'
-import { CategoriesRepository } from '../modules/cars/repositories/implementations/CategoriesRepository'
 import { createCategoryController } from '../modules/cars/useCases/createCategory'
 import { listCategoriesController } from '../modules/cars/useCases/listCategories'
 
 const categoriesRouter = Router()
-const categoriesRepository = new CategoriesRepository()
 
 categoriesRouter.post('/', (request, response) => {
   return createCategoryController.handle(request, response)
