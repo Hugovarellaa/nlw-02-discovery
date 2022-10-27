e17b5dfc053325f4d3965f5357610821e13dddf1
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-10-27 14:12:03 -0300

    Criando model de Category

--- server/src/routes/categories.routes.ts
+++ server/src/routes/categories.routes.ts
@@ -1,23 +1,23 @@
 import { Router } from 'express'
-import { v4 as uuidV4 } from 'uuid'
+import { Category } from '../model/Category'
 
 const categoriesRouter = Router()
 
-const categories = []
+const categories: Category[] = []
 
 categoriesRouter.post('/', (request, response) => {
   const { name, description } = request.body
 
-  const account = {
-    id: uuidV4(),
+  const category = new Category()
+  Object.assign(category, {
     name,
     description,
-    createdAt: new Date(),
-  }
+    created_at: new Date(),
+  })
 
-  categories.push(account)
+  categories.push(category)
 
-  return response.status(201).json({})
+  return response.status(201).json({ category })
 })
 
 export { categoriesRouter }
