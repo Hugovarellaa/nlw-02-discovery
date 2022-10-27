5efd28de21ac0c01983d74bf2b66c8c26d82e1ac
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-10-27 11:06:34 -0300

    Configurando ts-node-dev + typescript

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,10 @@
-import express from "express"
+import express from 'express'
 
 const app = express()
 app.use(express.json())
 
-app.get("/", (req, res) => {
-  return res.json({ message: "Ola mundo" })
+app.get('/', (req, res) => {
+  return res.json({ message: 'Ola mundo novamente' })
 })
 
-app.listen(3333, () => console.log("Server is running in port 3333"))
+app.listen(3333, () => console.log('Server is running in port 3333'))
