a092728fd09a322f03689f0ae09acd936e7aacc1
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-10-27 16:42:32 -0300

    Refatorando e implmentando com subtipos

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 import { categoriesRouter } from './routes/categories.routes'
+import { specificationRoutes } from './routes/specification.routes'
 
 const app = express()
 app.use(express.json())
 app.use('/categories', categoriesRouter)
+app.use('/specification', specificationRoutes)
 
 app.get('/', (req, res) => {
   const { name } = req.body
