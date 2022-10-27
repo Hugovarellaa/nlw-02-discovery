91b6c233e9363e563a2e88ce96de8fc395e9f802
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-10-27 15:20:11 -0300

    CreateCategoryService e refatorando

--- server/src/routes/categories.routes.ts
+++ server/src/routes/categories.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import { CategoriesRepository } from '../repositories/CategoriesRepository'
+import { CreateCategoryService } from '../services/CreateCategoryService'
 
 const categoriesRouter = Router()
 const categoriesRepository = new CategoriesRepository()
@@ -7,12 +8,9 @@ const categoriesRepository = new CategoriesRepository()
 categoriesRouter.post('/', (request, response) => {
   const { name, description } = request.body
 
-  const categoryAlreadyExists = categoriesRepository.findByName(name)
+  const createCategoryService = new CreateCategoryService(categoriesRepository)
+  createCategoryService.execute({ name, description })
 
-  if (categoryAlreadyExists) {
-    return response.status(400).json({ error: 'Category already exists' })
-  }
-  categoriesRepository.create({ name, description })
   return response.status(201).send()
 })
 
