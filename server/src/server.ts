b8c8bb3cd6cc7588d2ee1dac4467b3ea31b8e153
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-10-27 10:01:50 -0300

    First commit server

---
+++ server/src/server.ts
@@ -0,0 +1,10 @@
+import express from "express"
+
+const app = express()
+app.use(express.json())
+
+app.get("/", (req, res) => {
+  return res.json({ message: "Ola mundo" })
+})
+
+app.listen(3333, () => console.log("Server is running in port 3333"))
